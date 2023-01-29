1090069f0a77df33bf0da211f50f0d4f12f564db
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-30 02:22:09 +0300

    Задача №7. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом. Задача завершена.

---
+++ Ex_7/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача №7. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+
+Console.Write("Введите кол-во элементов массива: ");
+int x = int.Parse(Console.ReadLine()!);
+
+double[] array = new double[x];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = rand.Next(-990, 990)/10.0;
+}
+
+double min_number = array.Min();
+double max_number = array.Max();
+double differ = max_number - min_number;
+
+Console.Write("Сформированный массив: ");
+Console.WriteLine(String.Join(" | ", array));
+Console.Write($"Разница между максимальным элементом {max_number} и минимальным элементом {min_number} массива: {differ}");
