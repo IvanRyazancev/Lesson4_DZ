d2a05cb2c5a7b87ebc1fbb915c0d48a25c3aa09e
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-30 01:42:04 +0300

    Задача №6. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Задача выполнена.

---
+++ Ex_6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача №6. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Write("Введите кол-во элементов массива: ");
+int x = int.Parse(Console.ReadLine()!);
+
+int[] array  = new int[x];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+
+int[] newarray = new int[(array.Length / 2)];
+
+for (int i = 0; i < (array.Length /2); i++)
+{    
+    newarray[i] = array[i] * array[array.Length - 1 - i];
+}
+Console.Write("Сформированный массив: ");
+Console.WriteLine(String.Join(" ", array));
+Console.Write("Массив из произведений элементов родительского массива: ");
+Console.WriteLine(String.Join(" ", newarray));
