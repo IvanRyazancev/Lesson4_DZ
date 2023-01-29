6fa51c1560a0c966fa15d8649969130d301d4232
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-29 19:27:12 +0300

    Задача №2. Определить, присутствует ли в заданном массиве, некоторое число. Задача выполнена.

---
+++ Ex_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача №2. Определить, присутствует ли в заданном массиве, некоторое число.
+
+Console.Write("Введите число элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число для проверки наличия его в массиве: ");
+int CheckNumber = int.Parse(Console.ReadLine()!);
+
+
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{    
+    array[i] = new Random().Next(0, 100);         
+}
+
+Console.Write("Сформированный массив: ");
+Console.WriteLine(String.Join(" ", array));
+
+if (array.Contains(CheckNumber))
+{
+    Console.WriteLine($"Введеное число {CheckNumber} присутствует в сформированном массиве.");
+}
+else
+{
+    Console.WriteLine($"Введенное число {CheckNumber} не присутствует в сформированном массиве.");
+}
