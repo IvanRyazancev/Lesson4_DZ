5dd34872c506e16ad8ee42c8ec18698bbe797d38
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-29 21:51:43 +0300

    Задача №3.Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел. Задача выполнена.

---
+++ Ex_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача №3. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел.
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[n];
+
+int even = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {        
+        even ++;
+    }    
+}
+
+int odd = array.Length - even;
+
+Console.Write("Сформированный массив: ");
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine($"Кол-во четных элементов массива: {even}");
+Console.Write($"Кол-во нечетных элементов массива: {odd}");
