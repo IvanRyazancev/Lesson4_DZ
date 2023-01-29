520f1e2f4f6fa3ff370740bbc9eb45ef20676597
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-29 23:33:22 +0300

    Задача №4. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]. Завершение задачи.

---
+++ Ex_4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача №4. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+
+Console.Write("Введите число, для определения верхней границы диапазона массива: ");
+int x = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[123];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, x);
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine($"Кол-во элементов массива, лежащих в интервале [10, 99]: {count + 1}");
